d8db37c71ce05dcd525de238c1ea1d1c4ad44c7e
Matthias Büttgenbach <matthias.buettgenbach@cologne-intelligence.de>
2021-09-02 15:55:38 +0200

    uninstalled moment-timezone

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ const port = process.env.PORT
 
 const eventRouter = require('./routes/eventRoutes')
 
+app.use(cors())
 app.use(express.json())
 app.use(eventRouter)
-app.use(cors())
 
 app.listen(port, () => {
   console.log('Server is running on port ' + port)

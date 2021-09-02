355917dd199634922ed70da60257a1632e6171cc
Matthias Büttgenbach <matthias.buettgenbach@cologne-intelligence.de>
2021-09-02 15:50:54 +0200

    added cors middleware

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 require('./db/connection')
+const cors = require('cors')
 const express = require('express')
 const app = express()
 const port = process.env.PORT
@@ -8,6 +9,7 @@ const eventRouter = require('./routes/eventRoutes')
 
 app.use(express.json())
 app.use(eventRouter)
+app.use(cors())
 
 app.listen(port, () => {
   console.log('Server is running on port ' + port)
